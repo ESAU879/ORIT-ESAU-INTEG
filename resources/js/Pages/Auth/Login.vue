2b74605ca82d55820ca340d0ad2b4be6aed8f02b
ESAU879 <christianesauorit@gmail.com>
2024-06-07 15:58:30 +0800

    Initial commit

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,108 @@
+<script setup>
+import Checkbox from '@/Components/Checkbox.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+
+defineProps({
+    canResetPassword: {
+        type: Boolean,
+    },
+    status: {
+        type: String,
+    },
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+
+<template>
+    <GuestLayout>
+        <Head title="Log in" />
+        <div style="display: flex; justify-content: center">
+            <div style="display: flex; justify-content: center">
+    <!-- Making the logo bigger without specifying exact width and height -->
+    <img src="/images/wew.png" alt="Example Image" style="max-width: 80%; height: auto;" />
+</div>
+        </div>
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            <div>
+                <InputLabel for="email" value="Email" />
+
+                <TextInput
+                    id="email"
+                    type="email"
+                    class="mt-1 block w-full"
+                    v-model="form.email"
+                    required
+                    autofocus
+                    autocomplete="username"
+                />
+
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+
+                <TextInput
+                    id="password"
+                    type="password"
+                    class="mt-1 block w-full"
+                    v-model="form.password"
+                    required
+                    autocomplete="current-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link
+                    v-if="canResetPassword"
+                    :href="route('password.request')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Forgot your password?
+                </Link>
+
+                <PrimaryButton class="ms-4 bg-green-600 hover:text-green-200 text-white" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Log in
+                </PrimaryButton>
+            </div>
+
+            <div class="flex items-center justify-center mt-4">
+                <Link
+                    href="/register"
+                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark: rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                >
+                    Register here
+                </Link>
+            </div>
+        </form>
+    </GuestLayout>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,104 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <div class="topbar">
+            <!-- Logo -->
+            <div class="topbar-logo">
+                <img src="/images/logo.png" alt="Example Image" style="max-width: 100%; height: 150px;" />
+            </div>
+            <!-- Navigation Links -->
+            <nav class="topbar-nav">
+                <!-- Navigation Links Here -->
+            </nav>
+        </div>
+    
+    <AuthenticatedLayout>
+        
+        <aside id="sidebar" class="sidebar">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="pages-register.html">
+                        <i class="bi bi-card-list"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="pages-register.html">
+                        <i class="bi bi-card-list"></i>
+                        <span>Tenants Profile</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">
+                        <i class="bi bi-grid"></i>
+                        <span>Room Management</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">
+                        <i class="bi bi-grid"></i>
+                        <span>Bed Management</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">
+                        <i class="bi bi-grid"></i>
+                        <span>Utility Bills</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">
+                        <i class="bi bi-grid"></i>
+                        <span>Payment History</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">
+                        <i class="bi bi-grid"></i>
+                        <span>SMS Configuration</span>
+                    </a>
+                </li>
+
+            </ul>
+        </aside>
+    </AuthenticatedLayout>
+</template>
+
+<style scoped>
+.sidebar {
+    width: 350px;
+    line-height: 60px;
+    background-color: #f8f9fa;
+    padding: 50px;
+    border-right: 0.5px solid #dee2e6;
+}
+
+.nav-link {
+    display: flex;
+    align-items: center;
+    color: #212529;
+    text-decoration: none;
+    padding: 10px 0;
+}
+
+.nav-link:hover {
+    background-color: #e9ecef;
+}
+
+.nav-link i {
+    margin-right: 10px;
+}
+
+
+</style>
